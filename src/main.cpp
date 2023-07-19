f715fa9101f375c5e03ed1e38a0a5057439f749f
tobsec <tobias.claus@web.de>
2023-07-19 15:56:07 +0200

    First revision

---
+++ src/main.cpp
@@ -0,0 +1,168 @@
+#include <mcp_can.h>
+#include <SPI.h>
+
+// MCP2515 Pins
+#define CAN_CS_PIN 10u
+#define CAN_INT_PIN 2u
+
+// PWM Output
+#define PWM_PIN 3u
+
+// Amount of Sensors
+#define NUM_SENSORS 2u
+
+// CAN-ID und Data Length
+#define CAN_ID_0 0x180u
+#define CAN_ID_1 (CAN_ID_0 + 1u)
+#define CAN_DATA_LEN 8u
+#define CAN_MSG_TIMEOUT_MS 25u
+
+// PWM characteristic curve
+#define M -5.0f    // Slope
+#define B 99.5f    // Offset
+
+// Stoichiometric Air/Fuel Ratio
+#define STOICH_AFR 14.7f
+
+typedef struct uego_t
+{
+    float valueLambda;
+    float valueAfr;
+    unsigned long lastRx;
+    // boolean valid;
+} uego;
+
+MCP_CAN CAN0(CAN_CS_PIN);
+uego sensor[NUM_SENSORS];
+
+void checkCanReception(void);
+void updateOutput(void);
+
+void setup()
+{
+    Serial.begin(115200u);
+
+    // Init CAN
+    if ( CAN0.begin(MCP_STDEXT, CAN_500KBPS, MCP_16MHZ) == CAN_OK )
+    {
+        Serial.print("MCP2515 Init Okay!!\r\n");
+    }
+    else
+    {
+        Serial.print("MCP2515 Init Failed!!\r\n");
+        while(1);
+    }
+
+    // Setting pin 2 for /INT input
+    pinMode(PWM_PIN, INPUT);
+
+    // CAN0.init_Mask(0, 1, 0x00FFFF00);                                // Init first mask...
+    // CAN0.init_Filt(0, 1, 0x00FFEE00);                                // Init first filter...
+    // CAN0.init_Filt(1, 1, 0x00FFDD00);                                // Init second filter...
+
+    // CAN0.init_Mask(1, 1, 0x00FFFF00);                                // Init second mask...
+    // CAN0.init_Filt(2, 1, 0x00FFBB00);                                // Init third filter...
+    // CAN0.init_Filt(3, 1, 0x00FFAA00);                                // Init fourth filter...
+    // CAN0.init_Filt(4, 1, 0x00FF8800);                                // Init fifth filter...
+    // CAN0.init_Filt(5, 1, 0x00FF7700);                                // Init sixth filter...
+
+    CAN0.setMode(MCP_NORMAL);                                // Change to normal mode to allow messages to be transmitted
+}
+
+void loop()
+{
+    checkCanReception();
+    updateOutput();
+}
+
+void checkCanReception(void)
+{
+    unsigned long rxId;
+    uint8_t len = 0u;
+    uint8_t rxBuf[8u];
+
+    // If pin 2 is low, read receive buffer
+    if(!digitalRead(2))
+    {
+        CAN0.readMsgBuf(&rxId, &len, rxBuf); // Read data: len = data length, buf = data byte(s)
+
+        if ( ( CAN_ID_0 == rxId ) &&
+             ( CAN_DATA_LEN == len ) )
+        {
+            // Interpret data e.g. Lambda 0.85 <-> 8500u <-> 0x 21 34
+            uint16_t lambdaInt = ((uint16_t)rxBuf[0] << 8) | rxBuf[1];
+            sensor[0u].valueLambda = 0.0001f * lambdaInt;
+            sensor[0u].valueAfr = sensor[0u].valueLambda * STOICH_AFR;
+            sensor[0u].lastRx = millis();
+        }
+        else if ( ( CAN_ID_1 == rxId ) &&
+                  ( CAN_DATA_LEN == len ) )
+        {
+            // Interpret data e.g. 0.85 -> 8500 -> 0x 21 34
+            uint16_t lambdaInt = ((uint16_t)rxBuf[0] << 8) | rxBuf[1];
+            sensor[1u].valueLambda = 0.0001f * lambdaInt;
+            sensor[1u].valueAfr = sensor[1u].valueLambda * STOICH_AFR;
+            sensor[1u].lastRx = millis();
+        }
+
+        // Serial.print("ID: ");
+        // Serial.print(rxId, HEX);
+        // Serial.print(" Data: ");
+        // for(int i = 0; i<len; i++)                     // Print each byte of the data
+        // {
+        //     if(rxBuf[i] < 0x10)                                // If data byte is less than 0x10, add a leading zero
+        //     {
+        //         Serial.print("0");
+        //     }
+        //     Serial.print(rxBuf[i], HEX);
+        //     Serial.print(" ");
+        // }
+        // Serial.println();
+    }
+}
+
+void updateOutput(void)
+{
+    float outputAfr = 0.0f;
+
+    // Check all sensors for invalid value or timeout
+    for (uint8_t i=0u; i<NUM_SENSORS; i++)
+     {
+        if ((sensor[i].valueAfr != 0.0f) && (sensor[i].lastRx > (millis() - CAN_MSG_TIMEOUT_MS)))
+        {
+            // Sensor is valid, take highest (leanest) value for output
+            // sensor[i].valid = true;
+            outputAfr = max(outputAfr, sensor[i].valueAfr); // Take highest (leanest) value for output
+        }
+        // else
+        // {
+        //     sensor[i].valid = false;
+        // }
+    }
+
+
+    if (outputAfr != 0.0f)
+    {
+        // At least one sensor is valid, calculate output according to characteristic curve
+        uint8_t pwmValue = (uint8_t)(M * outputAfr + B);
+        /*
+          +----+------+
+          | y  |  x   |
+          +----+------+
+          | 49 | 10.1 |
+          | 40 | 11.9 |
+          | 30 | 13.9 |
+          | 20 | 15.9 |
+          | 10 | 17.9 |
+          |  0 | 20.0 |
+          +----+------+
+        */
+        analogWrite(PWM_PIN, pwmValue);
+    }
+    else
+    {
+        // Error, no valid sensor
+        analogWrite(PWM_PIN, 0u);
+    }
+
+}
